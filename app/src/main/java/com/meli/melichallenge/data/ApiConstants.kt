c6b4840c955249d3fe3dc7e5423952393262e901
Camilo Villate <cvillate@hexacta.com>
2024-10-02 19:41:55 -0500

    Change the style of the row and add image

--- app/src/main/java/com/meli/melichallenge/data/ApiConstants.kt
+++ app/src/main/java/com/meli/melichallenge/data/ApiConstants.kt
@@ -2,5 +2,5 @@ package com.meli.melichallenge.data
 
 object ApiConstants {
   const val BASE_URL = "https://api.mercadolibre.com/"
-  const val SEARCH_ENDPOINT = "sites/MLA/search"
+  const val SEARCH_ENDPOINT = "sites/MCO/search"
 }
